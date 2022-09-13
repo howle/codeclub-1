5d138c31a2070a29eea97ec21dd74349fa99fcd2
john <king@mail.com>
2022-09-13 10:55:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 export default {
-  fetch(request) {
-    /*
-    // Respond based on if the URL of the request matches what is hardcoded in the following code block.
-    if (request.url == "https://codeclub-1.willsmithee.workers.dev") {
-      return new Response("Hello worker!", {
-        headers: {
-          "content-type": "text/plain",
-        },
-      });
-    } else {
-      return new Response("Error Worker!", {
-        headers: {
-          "content-type": "text/plain",
-        },
-      });
-    }
-    */
-    // Respond with a sentence containg the request's method, otherwise respond that the request object doesn'r exist
-    if (request) {
-      return new Response(`This request used the ${request.method} method.`);
-    } else {
-      return new Response(`The request object does not exist`);
-    }
-  },
-};
+fetch(request) {
+
+if (request)
+return new Response(`hello ${request.method} world.`);
+
+}
+}
